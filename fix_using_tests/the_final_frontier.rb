22877fb678a15f20f20b14873532aec6dc329a35
Juan Carlos Pleitez <jcpleitez@outlook.com>
2020-10-05 17:36:38 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -15,8 +15,9 @@ def greet_crew (crew)
   crew.each {|crew_member| "Hello #{crew_member}."}
 end
 
+date = generate_star_date
+
 def engage
-  date = generate_star_date
   state_log(date)
   greet_crew(crew)
 end
